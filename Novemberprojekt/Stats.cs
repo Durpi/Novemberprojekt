665c1a116c30f61a96c91c8f11ce20e89a130dcb
Pontus <02pontuskarlsson@gmail.com>
2020-12-16 10:05:41 +0100

    Final Adjustments

--- Novemberprojekt/Stats.cs
+++ Novemberprojekt/Stats.cs
@@ -5,6 +5,8 @@ namespace Novemberprojekt
     public class Stats
     {
         //Makes the variables that "Dish" and "Jury" both use
+
+        //Static because of not wanting the same number generated
         public static Random generator = new Random();
         
         public int salty;
